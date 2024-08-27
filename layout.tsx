b3f1e3bcff5c40744ade9b282ae287bb33c70e01
masumali23020 <mdmasum0218@gmail.com>
2024-08-27 22:27:01 +0600

    dashborad layout design

---
+++ layout.tsx
@@ -0,0 +1,5 @@
+const deshboredLayout = () => {
+  return <div>fsadfas</div>;
+};
+
+export default deshboredLayout;
---
+++ src/app/sign-in/page.tsx
@@ -0,0 +1,5 @@
+const LoginPage = () => {
+  return <div>sign ins</div>;
+};
+
+export default LoginPage;
