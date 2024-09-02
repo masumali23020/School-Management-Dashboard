dfca1907e2cf4bc772642e8ce05d7ea4de36fa5d
masumali23020 <mdmasum0218@gmail.com>
2024-09-02 07:33:52 +0600

    count Chart

--- src/app/(dashborad)/admin/page.tsx
+++ src/app/(dashborad)/admin/page.tsx
@@ -1,3 +1,4 @@
+import CountChart from "@/app/components/CountChart";
 import UserCards from "@/app/components/UserCards";
 
 const page = () => {
@@ -11,6 +12,20 @@ const page = () => {
           <UserCards type="parents" />
           <UserCards type="staff" />
         </div>
+        {/* middile chart  */}
+
+        <div className="flex gap-4 flex-col lg:flex-row">
+          {/* count chart  */}
+          <div className="w-full lg:w-1/3 h-[450px]">
+            <CountChart />
+          </div>
+
+          {/* attendence chart  */}
+          <div className="w-full lg:w-2/3 h-[450px]"></div>
+        </div>
+        {/* middile chart  */}
+
+        <div>buttom</div>
       </div>
       {/* right side  */}
       <div className="w-full lg:w-1/3">right</div>
