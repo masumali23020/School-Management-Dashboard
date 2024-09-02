70b3a087ed47bf8671dd5679009266c373f28fe6
masumali23020 <mdmasum0218@gmail.com>
2024-09-02 07:01:03 +0600

    admin panel user card

--- src/app/(dashborad)/admin/page.tsx
+++ src/app/(dashborad)/admin/page.tsx
@@ -1,5 +1,21 @@
+import UserCards from "@/app/components/UserCards";
+
 const page = () => {
-  return <div>admin</div>;
+  return (
+    <div className="flex gap-4 flex-col md:flex-row">
+      {/* left side  */}
+      <div className="w-full lg:w-2/3">
+        <div className="flex gap-4 justify-between flex-wrap">
+          <UserCards type="students" />
+          <UserCards type="teacher" />
+          <UserCards type="parents" />
+          <UserCards type="staff" />
+        </div>
+      </div>
+      {/* right side  */}
+      <div className="w-full lg:w-1/3">right</div>
+    </div>
+  );
 };
 
 export default page;
