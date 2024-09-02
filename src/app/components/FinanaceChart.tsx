cdba088c89349651e48648a3978be2591108f058
masumali23020 <mdmasum0218@gmail.com>
2024-09-02 15:31:41 +0600

    finaceChart

--- src/app/components/FinanaceChart.tsx
+++ src/app/components/FinanaceChart.tsx
@@ -1,6 +1,6 @@
 "use client";
-import Image from "next/image";
 
+import Image from "next/image";
 import {
   CartesianGrid,
   Legend,
@@ -16,73 +16,72 @@ const data = [
   {
     name: "Jan",
     income: 4000,
-    expences: 2400,
+    expense: 2400,
   },
   {
     name: "Feb",
     income: 3000,
-    expences: 1398,
+    expense: 1398,
   },
   {
     name: "Mar",
     income: 2000,
-    expences: 9800,
+    expense: 9800,
   },
   {
     name: "Apr",
     income: 2780,
-    expences: 3908,
+    expense: 3908,
   },
   {
     name: "May",
     income: 1890,
-    expences: 4800,
+    expense: 4800,
   },
   {
     name: "Jun",
     income: 2390,
-    expences: 3800,
+    expense: 3800,
   },
   {
-    name: "July",
+    name: "Jul",
     income: 3490,
-    expences: 4300,
+    expense: 4300,
   },
   {
     name: "Aug",
     income: 3490,
-    expences: 4300,
+    expense: 4300,
   },
   {
     name: "Sep",
     income: 3490,
-    expences: 4300,
+    expense: 4300,
   },
   {
     name: "Oct",
     income: 3490,
-    expences: 4300,
+    expense: 4300,
   },
   {
     name: "Nov",
     income: 3490,
-    expences: 4300,
+    expense: 4300,
   },
   {
     name: "Dec",
     income: 3490,
-    expences: 4300,
+    expense: 4300,
   },
 ];
 
 const FinanaceChart = () => {
   return (
-    <div className="bg-white rounded-lg p-4 h-full">
+    <div className="bg-white rounded-xl w-full h-full p-4">
       <div className="flex justify-between items-center">
-        <h1 className="text-lg font-semibold">Attendance</h1>
+        <h1 className="text-lg font-semibold">Finance</h1>
         <Image src="/moreDark.png" alt="" width={20} height={20} />
       </div>
-
       <ResponsiveContainer width="100%" height="90%">
         <LineChart
           width={500}
