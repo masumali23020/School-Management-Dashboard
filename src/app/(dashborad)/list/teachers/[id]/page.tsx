0a0af0b1a7408d66f21616978cabf9b2f4ce9db3
masumali23020 <mdmasum0218@gmail.com>
2024-09-03 22:59:32 +0600

    singleTeacherPage left side user info

---
+++ src/app/(dashborad)/list/teachers/[id]/page.tsx
@@ -0,0 +1,61 @@
+import Image from "next/image";
+
+const SingleTeacherPage = () => {
+  return (
+    <div className="flex p-4 gap-4 flex-col xl:flex-row">
+      {/* left side  */}
+      <div className="w-full xl:w-2/3">
+        {/* top  */}
+        <div className="flex flex-col lg:flex-row gap-4">
+          {/* user info card  */}
+          <div className="bg-lamaSky py-6 px-4 rounded-md flex-1 flex gap-4">
+            <div className="w-1/3">
+              <Image
+                src="https://images.pexels.com/photos/2182970/pexels-photo-2182970.jpeg?auto=compress&cs=tinysrgb&w=1200"
+                alt=""
+                width={144}
+                height={144}
+                className="w-36 h-36 rounded-full object-cover"
+              />
+            </div>
+            <div className="w-2/3 flex flex-col gap-4 justify-between">
+              <h2 className="text-xl font-semibold">Masum Billah </h2>
+              <p className="text-sm text-gray-500">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugit
+                adipisci praesentium{" "}
+              </p>
+              <div className="flex items-center justify-between gap-2 flex-wrap text-xs font-medium">
+                <div className="w-full md:w-1/3 lg:w-full 2xl:w-1/3 flex items-center gap-2">
+                  <Image src="/blood.png" alt="" width={14} height={14} />
+                  <span>A+</span>
+                </div>
+                <div className="w-full md:w-1/3 lg:w-full 2xl:w-1/3 flex items-center gap-2">
+                  <Image src="/date.png" alt="" width={14} height={14} />
+                  <span>January 2025 </span>
+                </div>
+                <div className="w-full md:w-1/3 lg:w-full 2xl:w-1/3 flex items-center gap-2">
+                  <Image src="/mail.png" alt="" width={14} height={14} />
+                  <span>user@gmail.com</span>
+                </div>
+                <div className="w-full md:w-1/3 lg:w-full 2xl:w-1/3 flex items-center gap-2">
+                  <Image src="/phone.png" alt="" width={14} height={14} />
+                  <span>+1 234 567</span>
+                </div>
+                </div>
+                
+            </div>
+          </div>
+
+          {/* small card  */}
+          <div className=""></div>
+        </div>
+
+        {/* buttton  */}
+        <div className="">Schudel</div>
+      </div>
+      <div className="w-full xl:w-1/3">r</div>
+      {/* left side  */}
+    </div>
+  );
+};
+export default SingleTeacherPage;
